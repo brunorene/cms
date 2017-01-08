f0aa2ae6fb1dbe4db2ce2057880a96b9a4c9cbb2
Bruno Rene Santos <brunorene@gmail.com>
2017-01-08 01:21:18 +0000

    First Commit

--- src/main/kotlin/pt/brene/ui/PersonUI.kt
+++ src/main/kotlin/pt/brene/ui/PersonUI.kt
@@ -1,15 +1,9 @@
 package pt.brene.ui
 
 import com.vaadin.annotations.Theme
-import com.vaadin.data.fieldgroup.BeanFieldGroup
-import com.vaadin.event.ShortcutAction
 import com.vaadin.server.VaadinRequest
 import com.vaadin.spring.annotation.SpringUI
-import com.vaadin.ui.Button
-import com.vaadin.ui.GridLayout
-import com.vaadin.ui.TextField
 import com.vaadin.ui.UI
-import pt.brene.controller.Person
 import pt.brene.controller.PersonRepository
 
 const val LIST_VIEW: String = "list"
